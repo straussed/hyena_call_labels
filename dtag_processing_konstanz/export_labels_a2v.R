aac86eefa6dc80bf8861801fa56b64d200d5f0e7
Eli <straussed@gmail.com>
2025-09-04 15:29:49 -0400

    no row names in output

--- dtag_processing_konstanz/export_labels_a2v.R
+++ dtag_processing_konstanz/export_labels_a2v.R
@@ -3,7 +3,7 @@
 export_labels_a2v <- function(parsed_audits, destination = getwd()){
   for(f in unique(parsed_audits$wav_file)){
     to_write <- parsed_audits[parsed_audits$wav_file == f,]
-    write.csv(to_write, file = paste0(destination, gsub(to_write$wav_file[1], pattern = '.wav', replacement = '.csv')))
+    write.csv(to_write, file = paste0(destination, gsub(to_write$wav_file[1], pattern = '.wav', replacement = '.csv',)), row.names = F)
   }
   print(paste0('Files exported to ', destination))
 }
