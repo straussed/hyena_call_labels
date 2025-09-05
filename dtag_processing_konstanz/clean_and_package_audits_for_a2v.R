a1bb75f4fbfcc2b4370abab142f3f8d3c3f434a2
Eli <straussed@gmail.com>
2025-09-04 21:12:30 -0400

    rename audit batch processing script

--- dtag_processing_konstanz/clean_and_package_audits_for_a2v.R
+++ dtag_processing_konstanz/clean_and_package_audits_for_a2v.R
@@ -1,7 +1,7 @@
 ################################################################################
 
-## This script parses and saves one audit done by Jana to be sent to Julian
-## to help set up his analysis pipeline with the hyena data. It's pretty short. 
+## This script parses and saves audits for fine tuning of animal2vec, using some 
+## helper functions for converting audit times to wav file times. 
 
 
 ## Written by Eli Strauss March 2024
@@ -11,19 +11,21 @@
 library(dplyr)
 library(intervals)
 
-label_files <- list.files('~/Dropbox/Documents/Research/Partial_projects/CCAS/Labeling/Label_validaiton/finished_and_checked/', full.names = T)
+label_files <- list.files('~/Documents/code/hyena_call_labels/finished_and_checked/', full.names = T)
 
 ## SOURCE SOME CRITICAL FUNCTIONS
-source('/Volumes/cc23_eds/dtag_processing_konstanz/cue2wav.R')
-source('/Volumes/cc23_eds/dtag_processing_konstanz/cue2utc.R')
-source('/Volumes/cc23_eds/dtag_processing_konstanz/parse_audits.R')
-source('/Volumes/cc23_eds/dtag_processing_konstanz/export_labels_a2v.R')
+code_dir <- '~/Documents/code/hyena_call_labels/dtag_processing_konstanz/'
+source(paste0(code_dir, 'cue2wav.R'))
+source(paste0(code_dir, 'cue2utc.R'))
+source(paste0(code_dir, 'parse_audits.R'))
+source(paste0(code_dir, 'export_labels_a2v.R'))
 
 
-#THIS IS THE FOLDER WHERE THE CUE TABLES (wavcues) ARE
+#THIS IS THE FOLDER WHERE THE CUE TABLES (wavcues) ARE. THESE ARE EXPORTED FROM /cc23
+source(paste0(code_dir, 'export_cue_tables.R'))
 cuetab_folder <- '/Volumes/cc23_eds/cc23_cue_tables_csv/' 
 julian_labels <- parse_audits(label_files, cuetab_folder)
-julian_labels$time = julian_labels$wav_time
+julian_labels$time <- julian_labels$wav_time
 
 
 # TIDY UP TYPOS ETC
@@ -37,16 +39,15 @@ julian_labels$label <- gsub(x = julian_labels$label, pattern = ' \\]', replaceme
 
 # FILTER TO ONLY CALL TYPES OF INTEREST
 labels_to_use <- c('fed', 'sql', 'str', 'grn', 'whp', 'rum', 'gig', 'gwl', 'oth', 'unk', 
-                   'skp', 'nnf', 'soa', 'eoa')
+                   'skp', 'nnf', 'snr', 'soa', 'eoa')
 
 
 
 # visually inspect labels without any of these in them. Resolve typos or confirm that the rest can be deleted
 unique(julian_labels$label[!grepl(pattern = paste(labels_to_use, collapse = '|'), x = julian_labels$label)])
 
-# remove labels that dont match - removes 147 labels
+# remove labels that dont match - removes 177 labels
 nrow(julian_labels) - nrow(julian_labels[grepl(pattern = paste(labels_to_use, collapse = '|'), x = julian_labels$label),])
 julian_labels <- julian_labels[grepl(pattern = paste(labels_to_use, collapse = '|'), x = julian_labels$label),]
 
-
-export_labels_a2v(julian_labels, '/Users/straussed/Dropbox/Documents/Research/Partial_projects/CCAS/Labeling/audio_2_vec_labels/fine_tune_labels_Sep_28/')
+export_labels_a2v(julian_labels, destination = paste0(code_dir, '../hyena_a2v_labels/'))
